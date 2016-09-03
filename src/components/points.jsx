a8c633f811ea44eba2edcc7954abaa7bc1282c8e
Ola Jigin <ola.jigin@gmail.com>
2016-09-03 14:15:55 +0200

    Added json file and simple table for highscore

--- src/components/points.jsx
+++ src/components/points.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 //import { Button, Cell, Card, CardTitle, CardText, CardActions, DataTable, TableHeader, Grid} from 'react-mdl';
 import { Row, Col, Card, Table, CardTitle, Image } from 'react-materialize';
+import {trips} from '../data/trips.jsx';
 
 export default class Points extends Component {
-	
-
-	constructor(){
+		constructor(){
 		super();
 	}	
-	
 	render() {
+		trips.forEach(function (trip){
+			console.log(trip.points);
+		});
+
 		return (
 			<Row>
 				<Col s={12} m={12} l={6}>
-					 <Card className='blue-grey' textClassName='white-text'>
+					 <Card className='white'>
 					 <h2 className="center-text">Highscore</h2>
     					<Table centered bordered>
   							<thead>
@@ -25,22 +27,36 @@ export default class Points extends Component {
   							<tbody>
     							<tr>
       							<td>Ola</td>
-      							<td>200</td>
+      							<td>{trips[0].points}</td>
    							 </tr>
 
     							<tr>
-      							<td>Ola</td>
-      							<td>200</td>
+      							<td>Daniel</td>
+      							<td>{trips[1].points}</td>
+   							 </tr>
+    							<tr>
+      							<td>Pontus</td>
+      							<td>{trips[2].points}</td>
    							 </tr>
 
     							<tr>
-      							<td>Ola</td>
-      							<td>200</td>
+      							<td>Tova</td>
+      							<td>{trips[3].points}</td>
    							 </tr>
 
     							<tr>
-      							<td>Ola</td>
-      							<td>200</td>
+      							<td>Jigin</td>
+      							<td>{trips[4].points}</td>
+   							 </tr>
+
+    							<tr>
+      							<td>Sjövall</td>
+      							<td>{trips[5].points}</td>
+   							 </tr>
+
+    							<tr>
+      							<td>Karl</td>
+      							<td>{trips[6].points}</td>
    							 </tr>
   							</tbody>
 							</Table>
