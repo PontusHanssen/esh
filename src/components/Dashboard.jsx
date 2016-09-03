b3c6d0b5d5e9746510646c18985c8bfd5a0b7123
Pontus Persson <persson@flaskpost.org>
2016-09-03 13:09:19 +0200

    Migrated Trips.jsx to materialize

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-materialize';
-//import Trip from './Trip';
+import { Row } from 'react-materialize';
+import Trip from './Trip';
 //import Fuel from './fuel';
 //import Points from './points';
 
 export default class Dashboard extends Component {
   render() {
-    /*
+    
     return (
       <div style={{width: '80%', margin: 'auto'}}>
-        <Grid className={'dashboardGrid'}>
+        <Row className={'dashboardGrid'}>
           <Trip />
-          <Fuel />
-          <Points />
-        </Grid>
+        </Row>
       </div>
     );
-    */
+   
    return (<div>works</div>);
   }
 }
