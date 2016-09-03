290683f28d1ef2bcb599daa49b8d3b65dfa4d37e
Tova Linder <tovli744@student.liu.se>
2016-09-03 12:19:13 +0200

    Changed diagram

--- src/components/fuel.jsx
+++ src/components/fuel.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import {Card, CardTitle, CardText, CardActions, CardMenu, IconButton, DataTable, TableHeader} from 'react-mdl';
-import {LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line} from 'recharts';
+import {Card, CardTitle, CardText, CardActions, CardMenu, IconButton, DataTable, TableHeader, Button, Cell} from 'react-mdl';
+import {AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area} from 'recharts';
 export default class Fuel extends Component {
   render() {
-  	var data = [{name: '2016-01-01', value: 10}, {name: '2016-01-02', value: 30}, {name: '2016-01-03', value: 15}];
+  	var data = [{name: 'mon', value: 10}, {name: 'tue', value: 30}, {name: 'wed', value: 15}, 
+    {name: 'thur', value: 45}, {name: 'fri', value: 15}, {name: 'sat', value: 80}, {name: 'sun', value: 15}];
 
     return (
-      <div>
-     	<Card shadow={0} style={{width: '100%', margin: 'auto'}}>
+      <Cell phone={1} tablet={4}>
+     	<Card shadow={0}>
     	<CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.publicdomainpictures.net/pictures/50000/velka/fuel-gauge.jpg) center / cover'}}>Fuel Consumption</CardTitle>
     	<CardText>
-    	<LineChart width={500} height={200} data={data}
+    	<AreaChart width={500} height={200} data={data}
   			margin={{ top: 5, right: 50, left: 5, bottom: 5 }}>
   			<XAxis dataKey="name" />
   			<YAxis />
   			<CartesianGrid strokeDasharray="3 3" />
   			<Tooltip />
-  			<Legend />
-  			<Line type="monotone" dataKey="value" stroke="#8884d8" />
-		</LineChart>
+  			<Area type="monotone" dataKey="value" stroke="#8884d8" />
+		</AreaChart>
     	
     	</CardText>
 		</Card>
-      </div>
+    </Cell>
     );
   }
 }
