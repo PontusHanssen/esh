9c8b4518a010687866441a3c6f45c99701c512fd
Tova Linder <tovli744@student.liu.se>
2016-09-03 13:32:38 +0200

    Fuel component works

--- src/components/fuel.jsx
+++ src/components/fuel.jsx
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import {Card, CardTitle, CardText, CardActions, CardMenu, IconButton, DataTable, TableHeader, Button, Cell} from 'react-mdl';
+import {Card, CardTitle, CardPanel, Button, Col} from 'react-materialize';
 import {AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area} from 'recharts';
-export default class Fuel extends Component {
-  render() {
-  	var data = [{name: 'mon', value: 10}, {name: 'tue', value: 30}, {name: 'wed', value: 15}, 
-    {name: 'thur', value: 45}, {name: 'fri', value: 15}, {name: 'sat', value: 80}, {name: 'sun', value: 15}];
 
+export default class Fuel extends Component {
+  render() { 
+  
+  var data = [{name: 'mon', value: 10}, {name: 'tue', value: 30}, {name: 'wed', value: 15}, 
+    {name: 'thur', value: 45}, {name: 'fri', value: 15}, {name: 'sat', value: 80}, {name: 'sun', value: 15}];
+  
     return (
-      <Cell phone={1} tablet={4}>
-     	<Card shadow={0}>
-    	<CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.publicdomainpictures.net/pictures/50000/velka/fuel-gauge.jpg) center / cover'}}>Fuel Consumption</CardTitle>
-    	<CardText>
-    	<AreaChart width={500} height={200} data={data}
-  			margin={{ top: 5, right: 50, left: 5, bottom: 5 }}>
-  			<XAxis dataKey="name" />
-  			<YAxis />
-  			<CartesianGrid strokeDasharray="3 3" />
-  			<Tooltip />
-  			<Area type="monotone" dataKey="value" stroke="#8884d8" />
-		</AreaChart>
-    	
-    	</CardText>
-		</Card>
-    </Cell>
+      <Col s={12} m={8} l={4}>
+      <Card>
+      <CardTitle image="fuel.jpg">Fuel Consumption</CardTitle>
+      <AreaChart width={400} height={150} data={data}
+        margin={{ top: 20, right: 50, left: 5, bottom: 5 }}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area type="monotone" dataKey="value" stroke="#8884d8" />
+      </AreaChart>
+      </Card>
+    </Col>
     );
   }
 }
