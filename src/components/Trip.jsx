a99c575f4110dd96c5bac5677dae223293f06618
Pontus Persson <persson@flaskpost.org>
2016-09-03 13:34:36 +0200

    Larger text

--- src/components/Trip.jsx
+++ src/components/Trip.jsx
@@ -3,18 +3,21 @@ import {Card, CardTitle, Col} from 'react-materialize';
 export default class Trip extends Component {
   render() {
     return(
-      <Col l={4} s={12} m={6}>
+      <Col l={6} s={12} m={6}>
         <Card header={
-          <CardTitle style={{height: "200px"}} image={
-            "https://maps.googleapis.com/maps/api/staticmap?size=480x200&key=AIzaSyD3moXf7DqfjHWQdAtGD-3Iajitw-J2ANw&path=color:0xff0000ff%7Cweight:5%7C40.737102,-73.990318%7C40.749825,-73.987963%7C40.752946,-73.987384%7C40.755823,-73.986397"
-          }> 15min Trip </CardTitle>
+          <CardTitle style={{color: "#000"}} image={
+            "https://maps.googleapis.com/maps/api/staticmap?scale=2&size=400x150&key=AIzaSyD3moXf7DqfjHWQdAtGD-3Iajitw-J2ANw&path=color:0xff0000ff%7Cweight:5%7C40.737102,-73.990318%7C40.749825,-73.987963%7C40.752946,-73.987384%7C40.755823,-73.986397"
+          }></CardTitle>
         }
         >
+          <h1>15 min trip</h1>
+          <h4>
           2016-09-03 10:03 <br />
           15 min Trip<br />
           10 km <br />
-          Avg. consumption 0.5l/km <br />
+          Fuel 0.5 l/km <br />
           <strong>Points: 257 </strong>
+        </h4>
         </Card>
       </Col>
 
