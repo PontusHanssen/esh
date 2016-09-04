88d6d10a653581bde89e74692d29c767a3e1e3e2
Daniel Sjövall <dsjovall@gmail.com>
2016-09-04 09:48:51 +0200

    CSSSSSS

--- src/app.jsx
+++ src/app.jsx
@@ -27,7 +27,7 @@ class App extends Component {
           <div className="container">
             <div className="nav-wrapper">
               <a className="page-title">
-                KurBit
+                CurBit
               </a>
             </div>
           </div>
--- src/components/DashTrip.jsx
+++ src/components/DashTrip.jsx
@@ -13,7 +13,7 @@ export default class Trip extends Component {
   render(){
     this.trip = this.trips[Math.floor(Math.random() * this.trips.length)];
 
-    this.trip.img = "https://maps.googleapis.com/maps/api/staticmap?size=480x180&key=AIzaSyD3moXf7DqfjHWQdAtGD-3Iajitw-J2ANw&path=color:0xff0000ff" + this.trip.mapPoints.map((point) => 
+    this.trip.img = "https://maps.googleapis.com/maps/api/staticmap?size=480x180&key=AIzaSyD3moXf7DqfjHWQdAtGD-3Iajitw-J2ANw&path=color:0xff0000ff" + this.trip.mapPoints.map((point) =>
       `${point.lat},${point.long}`
     ).join('|');
     return(
@@ -23,12 +23,12 @@ export default class Trip extends Component {
         }
         actions={[<a onClick={() => this.setState({})}>Previous Trip</a>, <span className={"pull-right"}><a onClick={() => this.setState({})}>Next Trip</a></span>]}
       >
-        <ul>
-          <li><i className="material-icons" style={{'vertical-align': 'middle'}}>event</i>{this.trip.time}</li>
-          <li><i className="material-icons" style={{'vertical-align': 'middle'}}>schedule</i>{this.trip.duration} minutes</li>
-          <li><i className="material-icons" style={{'vertical-align': 'middle'}}>place</i>{this.trip.distance} km </li>
-          <li><i className="material-icons" style={{'vertical-align': 'middle'}}>ev_station</i>{this.trip.consumption} l/km</li>
-          <li><strong><i className="material-icons" style={{'vertical-align': 'middle'}}>star</i>{this.trip.points} points</strong></li>
+        <ul className="dashtrip-ul">
+          <li><i className="material-icons trip-li">event</i>{this.trip.time}</li>
+          <li><i className="material-icons trip-li">schedule</i>{this.trip.duration} minutes</li>
+          <li><i className="material-icons trip-li">place</i>{this.trip.distance} km </li>
+          <li><i className="material-icons trip-li">ev_station</i>{this.trip.consumption} l/km</li>
+          <li><strong><i className="material-icons trip-li">star</i>{this.trip.points} points</strong></li>
         </ul>
       </Card>
     </Col>
@@ -40,4 +40,3 @@ export default class Trip extends Component {
 
 
 58.384833, 15.542284
-
